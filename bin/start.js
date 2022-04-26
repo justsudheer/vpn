d2c55a97a4fffe77beeff197bf222358da51cc19
Sudheer Singh <sudheersingh@Sudheers-MacBook-Pro-2.local>
2022-04-27 03:06:41 +0530

    remove init

--- bin/start.js
+++ bin/start.js
@@ -2,7 +2,7 @@ const Server = require('../server');
 const initWiregurad = require('./init');
 
 // setTimeout(() => {
-    initWiregurad().then(console.log('WireGuard initialized'))
+   // initWiregurad().then(console.log('WireGuard initialized'))
 // }, 120000);
 
 Server.start(3000);
